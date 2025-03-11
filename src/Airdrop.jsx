0bbc6032eed4155ccf265255c7eadaa1698a2a7a
ksmbhv <ksupadhyaya27@gmail.com>
2025-03-11 22:44:00 +0530

    adding wallet now

---
+++ src/Airdrop.jsx
@@ -0,0 +1,7 @@
+export function Airdrop() {
+    return <div>
+        hi from airdrop
+        <input type="text" placeholder="Enter your address"></input>
+        <button>Request Airdrop</button>
+    </div>
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
+/* import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+// import { SendTokens } from './SendTokens';
+// import { SignMessage } from './SignMessage';
+*/
+import { Airdrop } from './Airdrop';
 
 function App() {
+  //const network = WalletAdapterNetwork.Devnet;
+
+  //const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
   return (
     <>
-    <div>
-    Hii</div>
+      <div>
+        Hi There
+      </div>
+      <Airdrop></Airdrop>
     </>
-  )
+  );
 }
 
-export default App;
+export default App
